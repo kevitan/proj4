49a27a55b6d7c1abed477915521718cf02b945ab
kevitan <kevitan@umich.edu>
2012-11-17 15:43:23 -0500

    Update test.java

--- test.java
+++ test.java
@@ -2,6 +2,6 @@ package eecs285.kevitan.proj4;
 
 public class test {
     public static void main( String args[]){
-        System.out.println("brohoof");
+        System.out.println("go go go");
     }
 }
