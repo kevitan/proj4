d72c6f1adf21a5465a4f10198af1f93622c5123a
Shannon <scpawl@umich.edu>
2012-11-20 23:35:06 -0500

    simply created Listener.java, GameWindow.java, Go285.java

---
+++ Go285.java
@@ -0,0 +1,19 @@
+package eecs285.proj4;
+
+/*
+	Project 4: Go285
+	@author Shannon Pawloski, Kevin Tan, David Rushdoony, Michael Noonan
+	@date Nov 20, 2012
+	Go285.java
+	Contains the main function.
+*/
+
+public class Go285
+{
+	public static void main(String [] args)
+	{
+		GameWindow game;
+		game = new GameWindow();
+		
+	}
+}
